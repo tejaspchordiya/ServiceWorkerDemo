29e74543d601bff7dec97f77254cd94229c6436d
tejaspchordiya <tejaspchordiya@gmail.com>
2020-01-14 16:21:38 +0530

    Added SVG files

--- sw.js
+++ sw.js
@@ -1,60 +1,22 @@
-var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = [
-    '/',
-    '/lib/require.js',
-    '/lib/jquery-1.9.1.js',
-    '/lib/underscore.js'
-];
+self.addEventListener( 'install', event => {
+    console.log( 'V1 installing…' );
 
-// Here, this means window
-this.addEventListener( 'install', function( event ) {
-    // Perform install steps
+// cache a cat SVG
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function(cache) {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
+        caches.open( 'static-v1' ).then( cache => cache.add( '/cat.svg' ) )
     );
 } );
 
-this.addEventListener( 'activate', function( event ) {
-    console.log( 'Service Worker activating.' );
+self.addEventListener( 'activate', event => {
+    console.log( 'V1 now ready to handle fetches!' );
 } );
 
-/*this.addEventListener( 'fetch', function( event ) {
+self.addEventListener( 'fetch', event => {
+    const url = new URL( event.request.url );
 
-    //console.log( window );
-    console.log( " Service worker :: Inside fetch " + event );
-    event.respondWith(
-        window.caches.match( event.request )
-            .then( function( response ) {
-                // Cache hit - return response
-                if ( response ) {
-                    return response;
-                }
-
-                return fetch( event.request ).then(
-                    function( response ) {
-                        // Check if we received a valid response
-                        if ( !response || response.status !== 200 || response.type !== 'basic' ) {
-                            return response;
-                        }
-
-                        // IMPORTANT: Clone the response. A response is a stream
-                        // and because we want the browser to consume the response
-                        // as well as the cache consuming the response, we need
-                        // to clone it so we have two streams.
-                        var responseToCache = response.clone();
-
-                        window.caches.open( 'my-site-cache-v1' )
-                            .then( function( cache ) {
-                                cache.put( event.request, responseToCache );
-                            } );
-
-                        return response;
-                    }
-                );
-            } )
-    );
-} );*/
+// serve the cat SVG from the cache if the request is
+// same-origin and the path is '/dog.svg'
+    if ( url.origin == location.origin && url.pathname == '/dog.svg' ) {
+        event.respondWith( caches.match( '/cat.svg' ) );
+    }
+} );
